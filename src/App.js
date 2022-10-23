b58340140ff9bb4154efce7011d19cbfcdfc2665
lohpaul9 <lohpaul9@seas.upenn.edu>
2022-10-23 14:36:10 -0400

    for learners

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const notes = [
+    'Hello!',
+    "my",
+    "name",
+    "is",
+    "Paul!"
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListOfThings notes = {notes}/>
     </div>
   );
 }
+
+
+const ListOfThings = ({notes}) => {
+
+  const jsxNotes = (note, index) =>  {
+      return <p>Note {index}: {note}</p>
+  }
+
+  const jsxArrayOfNotes = notes.map(jsxNotes)
+  
+  return (
+    <div>
+      {jsxArrayOfNotes}
+    </div>
+  )
+}
+
+
+
+const CounterApp = () => {
+
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => {setCounter(counter+1)}
+  
+  return (
+    <div>
+      <button onClick={increaseByOne}>Increment</button>
+      {counter}
+    </div>
+  )
+}
+
+const MessageForm = () => {
+  const [displayText, setDisplayText] = useState("")
+  const [field, setField] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setDisplayText(field)
+    setField("")
+  }
+
+  const handleChange = event => {
+    setField(event.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={field} onChange={handleChange}></input>
+        <button type='Submit'>Submit</button>
+      </form>
+      <p>The last message was: {displayText}</p>
+    </div>
+  )
+}
+
 
 export default App;
